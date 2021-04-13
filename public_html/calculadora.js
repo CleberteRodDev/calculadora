7d38bf56843cd64c79b684c2a1671b8dc0e6b169
Cleberte Rodrigues <cleberterodrigues.dev@gmail.com>
2021-04-13 14:13:53 -0300

    modificação rápida no JS para funcionar o botão de soma

--- public_html/calculadora.js
+++ public_html/calculadora.js
@@ -1,13 +1,9 @@
-var num = document.getElementById('numSoma')
-var num2 = document.getElementById('numSoma2')
-var res = document.getElementById('res')
-n1 = Number(num.value)
-n2 = Number(num2.value)
 function somar(s){
-    if(n1,n2 < 1)
-        alert('digite um número válido')
-    if
-        s = n1 +n2
-    
+    var num = document.getElementById('numSoma')
+    var num2 = document.getElementById('numSoma2')
+    var res = document.getElementById('res')
+    n1 = Number(num.value)
+    n2 = Number(num2.value)
+    s = n1 + n2
+    res.innerHTML = `A soma entre ${n1} + ${n2} é igual a ${s}`
 }
-res.innerHTML = `A soma entre ${n1} + ${n2} é igual a ${s}`
