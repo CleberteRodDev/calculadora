cdfbd672f3f90c1616157885e5e29f9b5daa470f
Cleberte Rodrigues <cleberterodrigues.dev@gmail.com>
2021-04-13 14:11:04 -0300

    adicionando novas propriedades aos códigos para a funcionalidade da calculadora

--- public_html/calculadora.js
+++ public_html/calculadora.js
@@ -1,1 +1,13 @@
-teste
+var num = document.getElementById('numSoma')
+var num2 = document.getElementById('numSoma2')
+var res = document.getElementById('res')
+n1 = Number(num.value)
+n2 = Number(num2.value)
+function somar(s){
+    if(n1,n2 < 1)
+        alert('digite um número válido')
+    if
+        s = n1 +n2
+    
+}
+res.innerHTML = `A soma entre ${n1} + ${n2} é igual a ${s}`
